8523532584e1d3b3b9d0cc5c02ceb81e246e6129
jinkimdata <jinkimdata@gmail.com>
2017-12-07 16:57:46 -0500

    content filled

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,22 +1,57 @@
 var dataproject = {
-    init: function() {},
+    init: function() {
+        dataproject.share();
+        dataproject.orgChart();
+    },
     share: function() {
-        $(".icon-twitter").on("click", function() {
-            var tweet = "";
-            var url = "";
+        $(".fa-twitter").on("click", function() {
+            var tweet = "Keep up-to-date with this overview of the Baltimore police racketeering cases.";
+            var url = "http://data.baltimoresun.com/news/gun-trace-overview/";
             var twitter_url = "https://twitter.com/intent/tweet?text=" + tweet + "&url=" + url + "&tw_p=tweetbutton";
             window.open(twitter_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
             return false;
         });
-        $(".icon-facebook").on("click", function() {
-            var picture = "";
-            var title = "";
-            var description = "";
-            var url = "";
+        $(".fa-facebook").on("click", function() {
+            var picture = "http://data.baltimoresun.com/news/gun-trace-overview/images/thumb.jpg";
+            var title = "Gun Trace Task Force Overview";
+            var description = "Keep up-to-date with this overview of the Baltimore police racketeering cases.";
+            var url = "http://data.baltimoresun.com/news/gun-trace-overview/";
             var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link=" + url + "&picture=" + picture + "&name=" + title + "&description=" + description + "&redirect_uri=http://www.facebook.com";
             window.open(facebook_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
             return false;
         });
+    },
+    orgChart: function() {
+        var prev;
+        var pos;
+        var containerPos;
+        getContainerPos();
+        window.addEventListener("resize", function() {
+            getContainerPos();
+        });
+        $(".profile, .overlay").one("click", function() {
+            $(".overlay").fadeOut();
+            console.log("trigger");
+        });
+        $(".profile, overlay").on("click", function() {
+            if ($(this).hasClass("active")) {
+                $(".profile.active .profile__content").css("left", 0);
+                $(".profile.active").removeClass("active");
+            } else {
+                $(".profile.active .profile__content").css("left", 0);
+                $(".profile.active").removeClass("active");
+                $(this).addClass("active");
+                pos = $(this).position();
+                $(".profile.active .profile__content").css("left", (pos.left - containerPos) * -1);
+            }
+        });
+        function getContainerPos() {
+            if ($(window).width() < 700) {
+                containerPos = 0;
+            } else {
+                containerPos = $(".orgChart").position().left;
+            }
+        }
     }
 };
 
--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,20 +1,20 @@
 var dataproject = {
 	init: function(){
-		//dataproject.share();
+		dataproject.share();
 		dataproject.orgChart();
 	},
 	share: function(){
-		$(".icon-twitter").on("click", function(){
-			var tweet = ""; //Tweet text
-			var url = ""; //Interactive URL
+		$(".fa-twitter").on("click", function(){
+			var tweet = "Keep up-to-date with this overview of the Baltimore police racketeering cases."; //Tweet text
+			var url = "http://data.baltimoresun.com/news/gun-trace-overview/"; //Interactive URL
 			var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
 			window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 		});
-		$(".icon-facebook").on("click", function(){
-			var picture = ""; //Picture URL
-			var title = ""; //Post title
-			var description = ""; //Post description
-			var url = ""; //Interactive URL
+		$(".fa-facebook").on("click", function(){
+			var picture = "http://data.baltimoresun.com/news/gun-trace-overview/images/thumb.jpg"; //Picture URL
+			var title = "Gun Trace Task Force Overview"; //Post title
+			var description = "Keep up-to-date with this overview of the Baltimore police racketeering cases."; //Post description
+			var url = "http://data.baltimoresun.com/news/gun-trace-overview/"; //Interactive URL
 	    	var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";    		
 			window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
 		});
@@ -27,7 +27,11 @@ var dataproject = {
 		window.addEventListener('resize', function() {
 			getContainerPos();
 		});
-		$('.profile').on('click',function() {
+		$('.profile, .overlay').one('click',function() {
+			$('.overlay').fadeOut();
+			console.log('trigger');
+		});
+		$('.profile, overlay').on('click',function() {
 			if($(this).hasClass('active')) {
 				$('.profile.active .profile__content').css('left', 0);
 				$('.profile.active').removeClass('active');
